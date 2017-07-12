67abed35588875ab14d7e54962ba38b97a6ab77e
Dima s <dmitriys9701@yandex.ru>
2017-07-12 17:09:18 +0300

    0.01

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*- 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 
 def start(bot, update):
-	update.message.reply_text('Введите свой рост и вес через через слэш с точностью до десятых долей')
-	update.message.reply_text('Пример - 170/66.4')
-def hello(bot, update):
-	update.message.reply_text(
-		'Hello {}'.format(update.message.from_user.first_name))
+	update.message.reply_text("Введите свой рост и вес через через слэш с точностью до десятых долей\nПример - 170/66.4")
+	
 
+def echo(bot, update):
+	resp = update.message.text
+	kbju = int(resp[0:2])*5-float(resp[4:])
+	update.message.reply_text(kbju)
+  
 updater = Updater('446638663:AAHkmiDnpX-YqLUrIRWOAfzIpJ4GmZZ8uB8')
-
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
